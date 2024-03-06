a30ea37ddb47014f95de5a8889fcaf5facc20da6
Aaron Askew <aaronaskew@gmail.com>
2024-03-05 19:14:56 -0600

    dynamic styles, attributes, and derived signals

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,32 @@ fn main() {
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
-    view! {
-        <button on:click=move |_| {
-            set_count.update(|n| *n += 1);
-        }>
+    let double_count = move || count() * 2;
 
-            "Click me: " // on stable, this is move || count.get();
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+
+            class=("red", move || count() % 2 == 1)
+        >
+            "Click me: "
             {count}
         </button>
+        <br/>
+        <progress
+            max="50"
+            // we use it once here
+            value=count
+        ></progress>
+        <br/>
+        <progress
+            max="50"
+            // we use it once here
+            value=double_count
+        ></progress>
+        <p>"Count: " {count}</p>
+        <p>"Double Count: " {double_count}</p>
     }
 }
